9850a5a7eb21267cae3445d74d50e282ee333a6a
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-18 19:52:34 +0300

    program 3 ranom generation, + doble

--- program_console_3/Program.cs
+++ program_console_3/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double number1 = new Random().Next(1, 100);
+System.Console.WriteLine(number1);
+double number2 = new Random().Next(1, 100);
+System.Console.WriteLine(number2);
+Console.ReadKey();
+
+System.Console.WriteLine(number1 / number2);
