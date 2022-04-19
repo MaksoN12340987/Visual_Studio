923e0264cfa8f31d0e08e8f20ac46979ac38f6d3
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-19 15:24:16 +0300

    Добавил вариативность способов нахождения

--- program_console_8/Program.cs
+++ program_console_8/Program.cs
@@ -1,17 +1,37 @@
-﻿//              0   1  2  3    4     5   6   7    8    9  10  11 12 13
-int[] array = { 9, 28, 1, 32, 1990, 16, 44, 39, 777, 334, 56, 1, 9, 7 };
+﻿//              0   1  2  3    4     5   6   7  8    9  10   11  12 13
+int[] array = { 9, 28, 1, 32, 1990, 16, 44, 39, 8, 334, 56, 777, 9, 7 };
+int n = array.Length;
 int find = 777;
 int index = 0;
 int indexFind = 0;
 
-while (index != 13)
+while (index < n)
 {
     if (array[index] == find)
     {
         indexFind = index;
+        // В таком случае бедет найден
+        // последний элемент, равный
+        // find
     }
     index++;
-    
+
 }
 
 System.Console.WriteLine(indexFind);
+
+// Иной способ:
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         System.Console.WriteLine(index);(В таком случае 
+//                                          отобразятся все элементы, 
+//                                          равные find)
+//         А если добавить:
+//         break;(В этом случае отобразится самый первый 
+//                найденый элемент, равный find)
+//     }
+//     index++;
+
+// }
