54c79de1af5868534b6b5d07d505bb32f2046f36
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-19 14:59:11 +0300

    Эврика

--- program_console_8/Program.cs
+++ program_console_8/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//              0   1  2  3    4     5   6   7    8    9  10  11 12 13
+int[] array = { 9, 28, 1, 32, 1990, 16, 44, 39, 777, 334, 56, 1, 9, 7 };
+int find = 777;
+int index = 0;
+int indexFind = 0;
+
+while (index != 13)
+{
+    if (array[index] == find)
+    {
+        indexFind = index;
+    }
+    index++;
+    
+}
+
+System.Console.WriteLine(indexFind);
