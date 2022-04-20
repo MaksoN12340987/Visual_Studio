9c0704a96ff8a45b77dad78f6f23a977e63894c0
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 13:59:36 +0300

    it good to home work

--- program_console_Home_Work_2/Program.cs
+++ program_console_Home_Work_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {2, 3, 7};
+
+int MaxElement(int[] collection)
+{
+    int index = 0;
+    int max = array[0];
+    int lenght = array.Length;
+    while (index < lenght)
+    {
+    if (array[index]>max)
+    {
+        max = array[index];
+    }
+    index++;
+    }
+    return max;
+}
+
+int MAX = MaxElement(array);
+System.Console.Write("Maximum element of the array = ");
+System.Console.WriteLine(MAX);
