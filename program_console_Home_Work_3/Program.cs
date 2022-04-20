9c9b3c139b402ff2b3c32e55ac4622b52128a45f
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 18:02:46 +0300

    it good to home work task 3

---
+++ program_console_Home_Work_3/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int parityNumber(int save)
+{
+    int parity = 0;
+    if (save % 2 == 0)
+    {
+        parity = 1;
+    }
+    return parity;
+}
+
+int PARINTY = parityNumber(number);
+if (PARINTY == 1)
+{
+    System.Console.WriteLine("number is even");
+}
+else
+{
+    System.Console.WriteLine("number is not even");
+}
+
