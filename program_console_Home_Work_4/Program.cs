8a1bca9558b3dcee78df10b7e96cdbedcc858a0a
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 19:30:08 +0300

    Не получается пока home work task 3

---
+++ program_console_Home_Work_4/Program.cs
@@ -0,0 +1,30 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+array[0] = 1;
+
+int ArrayFull(int[] collection)
+{
+    int index = 1;
+    while (index < n)
+    {
+        array[index]++;
+        index++;
+    }
+}
+
+int[] Array = new int[n];
+Array = ArrayFull(array);
+
+System.Console.WriteLine(Array);
+
+// int ParityNumber(int[] collection)
+// {
+//     int index = 0;
+//     int lenght = array.Length;
+//     int[] parityNumbers = {0};
+//     while (index < lenght)
+//     {
+
+//     }
+// }
+
