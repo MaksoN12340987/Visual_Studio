bb24a2e48154d7d4395a36edce9b0d529ea3b6c3
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 10:57:49 +0300

    пытаюсь собрать код, но чегото пока не догоняю

--- program_console_3/Program.cs
+++ program_console_3/Program.cs
@@ -1,7 +1,12 @@
-﻿double number1 = new Random().Next(1, 100);
-System.Console.WriteLine(number1);
-double number2 = new Random().Next(1, 100);
-System.Console.WriteLine(number2);
-Console.ReadKey();
+﻿// double number1 = new Random().Next(1, 100);
+// System.Console.WriteLine(number1);
+// double number2 = new Random().Next(1, 100);
+// System.Console.WriteLine(number2);
+System.Console.WriteLine("Введите первое число");
+var numberFirst = System.Console.ReadLine();
+System.Console.WriteLine("Введите второе число");
+var numberSecond = System.Console.ReadLine();
 
+int number1 = numberFirst;
+int number2 = numberSecond;
 System.Console.WriteLine(number1 / number2);
