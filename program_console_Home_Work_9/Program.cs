24ca07456f10b86c2d342988a708518be66dc6cb
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 22:20:07 +0300

    good nome work 9

---
+++ program_console_Home_Work_9/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Введите первое число");
+double numberA = Convert.ToDouble(System.Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+double numberB = Convert.ToDouble(System.Console.ReadLine());
+
+double MultiplicityNumbers(double num1, double num2)
+{
+    double multiple = 0;
+    if (num1 % num2 == 0)
+    {
+        System.Console.WriteLine("кратно");
+        System. Environment. Exit(0);
+    }
+    else
+    {
+        multiple = num1 % num2;
+    }
+    return multiple;
+}
+
+double numbersMultiplicity = MultiplicityNumbers(numberA, numberB);
+System.Console.Write("не кратно, остаток ");
+System.Console.Write(numbersMultiplicity);
