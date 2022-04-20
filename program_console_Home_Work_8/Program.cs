1930e76456fb3d1787d1c983bced1c556a486d38
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 21:56:28 +0300

    good nome work 8

---
+++ program_console_Home_Work_8/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine("Введите число из 3х знаков");
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+int RangeThird(int range)
+{
+    int ok = range;
+    if (range > 99 & range < 1000);
+    else
+    {
+        System.Console.WriteLine("Ошибка, введите число из 3х знаков");
+        System. Environment. Exit(0);
+    }
+    return ok;
+}
+
+int TwoNumber(int number)
+{
+    int twoNumber = 0;
+    number = number % 100;
+    twoNumber = number / 10;
+    return twoNumber;
+}
+
+int Third = RangeThird(a);
+
+int TWO = TwoNumber(Third);
+
+System.Console.WriteLine(TWO);
