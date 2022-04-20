8c4bc62b9ce5a87f697bc559a1c7a280e633a3c6
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 20:31:41 +0300

    Позже доделаю

--- program_console_Home_Work_4/Program.cs
+++ program_console_Home_Work_4/Program.cs
@@ -1,21 +1,35 @@
-﻿int n = Convert.ToInt32(Console.ReadLine());
+﻿System.Console.WriteLine("One numbers");
+int n = 4;
 int[] array = new int[n];
+
+System.Console.WriteLine(array[5]);
+
 array[0] = 1;
 
-int ArrayFull(int[] collection)
-{
-    int index = 1;
-    while (index < n)
-    {
-        array[index]++;
-        index++;
-    }
-}
+// int index = 1;
+// while (index < n)
+// {
+//     if (array[n] > array[index])
+//     {
+//         array[index]++;
+//     }
+//     index++;
+// }
 
-int[] Array = new int[n];
-Array = ArrayFull(array);
+// int ArrayFull(int[] collection)
+// {
+//     int index = 1;
+//     while (index < n)
+//     {
+//         array[index]++;
+//         index++;
+//     }
+// }
 
-System.Console.WriteLine(Array);
+// int[] Array = new int[n];
+// Array = ArrayFull(array);
+
+System.Console.WriteLine(array[2]);
 
 // int ParityNumber(int[] collection)
 // {
