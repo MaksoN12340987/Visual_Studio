6bb3e0d531065ee980c5ffbdb917d111658661b0
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 20:31:06 +0300

    Ну, вроде 6е получилось

---
+++ program_console_Home_Work_6/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+int lostNumber(int number)
+{
+    int numberLost = 0;
+    numberLost = number % 10;
+    return numberLost;
+}
+
+int lost = lostNumber(a);
+System.Console.WriteLine(lost);
