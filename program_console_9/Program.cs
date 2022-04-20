8f631b68b54f6d497d4d14932bb300fecc5dc5cd
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-20 10:56:07 +0300

    ока не дописал методы

---
+++ program_console_9/Program.cs
@@ -0,0 +1,22 @@
+﻿void FillArray(int[] collectionFill)
+{
+    int lengntFill = collection.Length;
+    int indexFill = 0;
+    while (indexFill < lengntFill)
+    {
+        collection[indexFill] = new Random().Next(1, 10);
+        indexFill++;
+    }
+}
+
+void PrintArray(int[] collectionPrint)
+{
+    int lengntPrint = collectionPrint.Length;
+    int indexPrint = 0;
+    while (indexFill < lengntPrint)
+    {
+        
+    }
+}
+
+int[] array = new int[10];
