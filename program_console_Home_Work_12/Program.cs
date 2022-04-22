4afc35961f248c8012b83cd9faf0ef98eca2e655
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-22 21:37:54 +0300

    good Home_Work 12

--- program_console_Home_Work_12/Program.cs
+++ program_console_Home_Work_12/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+void NumbeThree(int third)
+{
+    if (third > 99);
+    else
+    {
+        System.Console.WriteLine("enter a number greater than 99");
+        System. Environment. Exit(0);
+    }
+}
+
+int TwoNumber(int two)
+{
+    int save = (two / 10) % 10;
+    return save;
+}
+
+NumbeThree(number);
+int TWO = TwoNumber(number);
+System.Console.WriteLine(TWO);
