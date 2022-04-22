d098a7689c8525e82ae6d9ec4bc34f289b09ada2
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-22 16:34:45 +0300

    нифига не получается((((

--- program_console_Home_Work_5/Program.cs
+++ program_console_Home_Work_5/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿System.Console.Write("Enter n: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine();
+int index = (n * 2) + 1;
+int[] array = new int[index];
+array[0] = -n;
+
+void ArrayFull(int[] collection)
+{
+    int indexFill = 1;
+    int lengntFill = collection.Length;
+    int save = collection[0];
+    while (indexFill < lengntFill)
+    {
+        if (save < 0)
+        {
+            int indexSave = collection[(indexFill--)];
+            collection[indexFill] = collection[indexSave] + 1;
+            indexFill++;
+            save++;
+        }
+        if (save > 0)
+        {
+            
+            collection[indexFill] = collection[indexFill] + save;
+            indexFill++;
+            save++;
+        }
+        else
+        {
+            indexFill++;
+            save++;
+        }
+    }
+}
+
+// Добавил метод PrintArray только для проверки элементов массива
+void PrintArray(int[] collectionPrint)
+{
+    int lengntPrint = collectionPrint.Length;
+    int indexFill = 0;
+    while (indexFill < lengntPrint)
+    {
+        System.Console.WriteLine(collectionPrint[indexFill]);
+        indexFill++;
+    }
+}
+
+ArrayFull(array);
+PrintArray(array);
