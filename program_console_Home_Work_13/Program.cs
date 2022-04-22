c43c8bb7b805ea95dad9247d5b74306662772cac
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-22 23:13:22 +0300

    good Home_Work 13

--- program_console_Home_Work_13/Program.cs
+++ program_console_Home_Work_13/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("enter the number:");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+
+int ThirdNumber(int third)
+{
+    int save = 0;
+    int range = 1000;
+    int coll = 0;
+    int collend = 0;
+    int threeNumbers =100;
+    if (third < 100)
+    {
+        System.Console.WriteLine("No third digit");
+        System.Environment.Exit(0);
+    }
+    if (third > 99 & third < range)
+    {
+        save = third % 10;
+    }
+    else
+    {
+        while (third > range)
+        {
+            if (third < (range * 10))
+            {
+                coll = range;
+            }
+            range = range * 10;
+        }
+        collend = coll / threeNumbers;
+        save = (third / collend) % 10;
+    }
+    return save;
+}
+
+int third = ThirdNumber(number);
+System.Console.WriteLine(third);
---
+++ program_console_Home_Work_14/Program.cs
@@ -0,0 +1,3 @@
+﻿int a = 10000000 / 10000;
+int b = 999999 / a;
+System.Console.WriteLine(b);
