5e7d25c98a731b2e593c6ff14971ddff99beb27d
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-22 21:13:08 +0300

    good Home_Work 11

--- program_console_Home_Work_11/Program.cs
+++ program_console_Home_Work_11/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Уnter the first number: ");
+int numberA = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Уnter the second number: ");
+int numberB = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine();
+
+void Мultiple(int multiplesA, int multiplesB)
+{
+    if ((multiplesA * multiplesA) == multiplesB)
+    {
+        System.Console.WriteLine("The first number is the square of the second");
+        System. Environment. Exit(0);
+    }
+    if ((multiplesB * multiplesB) == multiplesA)
+    {
+        System.Console.WriteLine("The second number is the square of the first");
+        System. Environment. Exit(0);
+    }
+    else
+    {
+        System.Console.WriteLine("No");
+    }
+}
+
+Мultiple(numberA, numberB);
