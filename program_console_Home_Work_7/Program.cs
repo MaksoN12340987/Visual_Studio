190b5d25504e7fe488ad919fc914a14e79a2649d
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-22 21:11:13 +0300

    good Home_Work 7

--- program_console_Home_Work_7/Program.cs
+++ program_console_Home_Work_7/Program.cs
@@ -1,2 +1,27 @@
-﻿int a = new Random().Next(10, 100);
-System.Console.WriteLine(a);
+﻿int greatest = 0;
+
+int NumberRandom(int save)
+{
+    save = new Random().Next(10, 100);
+    return save;
+}
+
+int GreatestNumber(int number)
+{
+    int saveOne = number % 10;
+    int saveTwo = number / 10;
+    if (saveOne > saveTwo)
+    {
+        return saveOne;
+    }
+    else
+    {
+        return saveTwo;
+    }
+    
+}
+
+int temporary = NumberRandom(greatest);
+System.Console.WriteLine(temporary);
+greatest = GreatestNumber(temporary);
+System.Console.WriteLine(greatest);
