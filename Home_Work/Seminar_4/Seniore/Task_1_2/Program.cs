ba669bd94e20ca60badd47236c191573224b5647
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-26 20:19:29 +0300

    good task 1 & 2

---
+++ Home_Work/Seminar_4/Seniore/Task_1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// 1. Задать массив из 8 элементов и вывести их на экран
+
+int coll = 8;
+int[] array = new int[coll];
+
+void PrintArray(int[] collectionPrint)
+{
+    for (int i = 0; i < collectionPrint.Length; i++)
+    {
+        System.Console.WriteLine(collectionPrint[i]);
+    }
+}
+
+
+// 2. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FullArray(int[] collectionFull)
+{
+    for (int i = 0; i < collectionFull.Length; i++)
+    {
+        collectionFull[i] = new Random().Next(0, 2);
+    }
+}
+
+FullArray(array);
+PrintArray(array);
