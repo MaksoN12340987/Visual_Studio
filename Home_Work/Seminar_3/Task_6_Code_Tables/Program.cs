574b4ec50dc0efb05418dfdefd7e07f5cfb5920c
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-29 13:51:32 +0300

    good home work to seminar 3, task 6

---
+++ Home_Work/Seminar_3/Task_6_Code_Tables/Program.cs
@@ -0,0 +1,48 @@
+﻿// 6. Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N
+
+string enter = String.Empty;
+
+int GetValue(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        System.Console.Write("Enter the number: ");
+        getValue = System.Console.ReadLine();
+        correct = int.TryParse(getValue, out value);
+    }
+    return value;
+}
+
+void ArrayFull(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = (collection[i] + i) + 1;
+    }
+}
+
+void PrintArray(int[] collectionPrint)
+{
+    for (int i = 0; i < collectionPrint.Length; i++)
+    {
+        System.Console.WriteLine(collectionPrint[i]);
+    }
+}
+
+void SquareNumber(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = System.Convert.ToInt32(System.Math.Pow(mas[i], 2));
+    }
+}
+
+int n = GetValue(enter);
+int[] array = new int[n];
+ArrayFull(array);
+SquareNumber(array);
+PrintArray(array);
