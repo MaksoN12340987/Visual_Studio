8a0ffdcc728d915a45489f0264a6bc279b80d528
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-29 15:16:56 +0300

    good home work to seminar 3, task 2

---
+++ Home_Work/Seminar_3/Task_2_2D/Program.cs
@@ -0,0 +1,34 @@
+﻿// 2. Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 2D пространстве.
+
+string enterAX = "Ax";
+string enterAY = "Ay";
+string enterAZ = "Az";
+string enterBX = "Bx";
+
+double GetValue(string text)
+{
+    string getValue = String.Empty;
+    double value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        correct = double.TryParse(getValue, out value);
+    }
+    return value;
+}
+
+double Pifagor(double ax, double bx, double ay, double by)
+{
+    return System.Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2));
+}
+
+double Ax = GetValue(enterAX);
+double Ay = GetValue(enterAY);
+double Az = GetValue(enterAZ);
+double Bx = GetValue(enterBX);
+
+double result = Pifagor(Ax, Ay,Az,Bx);
+System.Console.WriteLine(result);
