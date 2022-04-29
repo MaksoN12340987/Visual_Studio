16611babc8fc50bf9bfd49b3d6c4ef73b5b2ddeb
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-04-29 11:44:37 +0300

    good home work to seminar 3, task 4

--- Home_Work/Seminar_3/Task_4_Palindrome/Program.cs
+++ Home_Work/Seminar_3/Task_4_Palindrome/Program.cs
@@ -1,44 +1,57 @@
 ﻿// 4. Напишите программу, которая принимает на вход пятизначное число и 
 // проверяет, является ли оно палиндромом.
 
-System.Console.Write("Enter the number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+string enter = String.Empty;
 
-// string enter = System.Console.ReadLine();
-
-// string NumderFive(string text)
-// {
-//     int number = Convert.ToInt32(text);
-//     if (number > 9999 & number < 100000) ;
-//     else
-//     {
-//         System.Console.WriteLine("enter a number greater than 99");
-//         System.Environment.Exit(0);
-//     }
-// }
-
-// string result = NumderFive(enter);
+int GetValue(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        System.Console.Write("Enter the number: ");
+        getValue = System.Console.ReadLine();
+        correct = int.TryParse(getValue, out value);
+    }
+    return value;
+}
 
 int Palindrome(int numberRal)
 {
-    int numberStsrt1 = numberRal / 10000;
-    int numberEnd1 = (numberRal % 100) / 10;
+    int numberStart1 = numberRal / 10000;
     int numberStsrt2 = (numberRal / 1000) % 10;
+    int numberEnd1 = (numberRal % 100) / 10;
     int numberEnd2 = (numberRal % 100) % 10;
-    int total = 1;
-    if (numberStsrt1 == numberEnd1)
+    int totalPal = 1;
+    if (numberStart1 == numberEnd2)
     {
-        if (numberStsrt2 == numberEnd2)
+        if (numberStsrt2 == numberEnd1)
         {
-            return total;
+            return totalPal;
         }
     }
     else
     {
-        total = -total;
-        return total;
+        totalPal = -totalPal;
     }
+    return totalPal;
 }
 
-int result = Palindrome(number);
-System.Console.WriteLine(result);
+string PrintAnswer(int answer)
+{
+    string text = String.Empty;
+    if (answer == 1)
+    {
+        text = "Yes";
+    }
+    else
+    {
+        text = "No";
+    }
+    return text;
+}
+
+int numberTrue = GetValue(enter);
+int result = Palindrome(numberTrue);
+System.Console.WriteLine(PrintAnswer(result));
