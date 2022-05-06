e608823a9aafe6221519d9c204c7f773347b43b4
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-05-06 17:52:29 +0300

    ((-__-))

--- Home_Work/Seminar_4/Seniore/Task_8_odd_position/Program.cs
+++ Home_Work/Seminar_4/Seniore/Task_8_odd_position/Program.cs
@@ -37,16 +37,5 @@ int SumOddPosition(int[] mas)
     return sum;
 }
 
-void PrintArray(int[] print)
-{
-    for (int i = 0; i < print.Length; i++)
-    {
-        System.Console.Write($"{print[i]} ");
-    }
-}
-
-int[] array = new int[GetValue(coll)];
-FullArray(array);
-PrintArray(array);
 System.Console.WriteLine();
 System.Console.WriteLine(SumOddPosition(array));
---
+++ Seninars/Max_Min/Program.cs
@@ -0,0 +1,22 @@
+﻿string MaxMinElement(int[] collection)
+{
+    int max = collection[0];
+    int min = collection[0];
+    string total = "";
+    for (int i = 0; i < collection.Length; i++)
+    {
+        if (collection[i] < min)
+        {
+            min = collection[i];
+        }
+        else
+        {
+            if (collection[i] > max)
+            {
+                max = collection[i];
+            }
+        }
+    }
+    total =$"Maximum element: {max}, Minimal element: {min}";
+    return total;
+}
