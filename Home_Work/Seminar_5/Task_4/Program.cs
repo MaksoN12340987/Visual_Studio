488fc0a7dd0a5b7373f43c40aeb9af7182e4e6e2
Maksin_Andreev <andrianov_maksim@outlook.com>
2022-05-06 23:35:55 +0300

    домашку немного доделал

---
+++ Home_Work/Seminar_5/Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿// Найти сумму чисел от 1 до А
+
+string enterMas = "Specify the number of elements in the array";
+
+int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
+
+void FillArray(int[] collection)
+{
+    int index = 0;
+    int lenght = collection.Length;
+    int[] mas = new int[lenght]; ;
+    while (index < lenght)
+    {
+        collection[index] = (collection[index] + index) + 1;
+        index++;
+    }
+}
+
+int[] array = new int[GetValueNullNatural(enterMas)];
+FillArray(array);
