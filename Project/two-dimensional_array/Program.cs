07b13f77d6dacf7659665fa4221e395d187bb4b5
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-05-10 23:39:04 +0300

    ((*--*))поправочки ошибок

---
+++ Project/two-dimensional_array/Program.cs
@@ -0,0 +1,25 @@
+﻿string enterMas = "Specify the number of elements in the array";
+string enterValue = "";
+
+int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
+
