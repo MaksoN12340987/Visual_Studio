6e5f628f20cb931151995c8adf4bd11966572374
Maksin_Andreev <andrianov_maksim@outlook.com>
2022-05-13 12:25:38 +0300

    доделывать нужно перевод систем счисления(*<>*)

---
+++ Project/Convertation/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+string enterM = "specify the number of array rows";
+
+int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
+
+int ConvertBinarySystem(int numer)
+{
+    int total = 0;
+    if (number < 10)
+    {
+        total = numer % 2;
+    }
+    else
+    {
+        for (int i = 0; i < numer; i++)
+        {
+            total = (numer % 2) *10;
+        }
+    }
+    return total;
+}
