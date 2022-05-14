86baaa9f9b98b5004260fa8dcea84555dd6e37ab
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-05-14 21:20:47 +0300

    Сделал несколько заданий и обновил HELP

---
+++ Home_Work/Seminar_6/1_Two-dimensional_array_of_integers/Program.cs
@@ -0,0 +1,57 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+string enterM = "specify the number of array rows";
+string enterN = "specify the number of array columns";
+
+int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
+
+void FillTwoDimensionalArray(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[j, i] =new System.Random().Next(-100, 101);
+        }
+    }
+}
+
+void PrintTwoArray(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            System.Console.Write($"{massive[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int M =  GetValueNullNatural(enterM);
+int N =  GetValueNullNatural(enterN);
+
+int[,] array = new int[M,N];
+
+FillTwoDimensionalArray(array);
+PrintTwoArray(array);
---
+++ Home_Work/Seminar_6/3_Two-dimensional_array_element_replacement/Program.cs
@@ -0,0 +1,74 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+string enterM = "specify the number of array rows";
+string enterN = "specify the number of array columns";
+
+int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
+
+void FillTwoDimensionalArray(int[,] mass)
+{
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[j, i] = new System.Random().Next(-100, 101);
+        }
+    }
+}
+
+void ReplacementElement(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[j, i] % 2 == 0)
+            {
+                matrix[j, i] *= -1;
+            }
+        }
+    }
+}
+
+void PrintTwoArray(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            System.Console.Write($"{massive[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int M =  GetValueNullNatural(enterM);
+int N =  GetValueNullNatural(enterN);
+
+int[,] array = new int[M,N];
+
+FillTwoDimensionalArray(array);
+PrintTwoArray(array);
+ReplacementElement(array);
+System.Console.WriteLine();
+PrintTwoArray(array);
