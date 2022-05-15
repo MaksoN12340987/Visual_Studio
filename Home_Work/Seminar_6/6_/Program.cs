7a0a460cfd7fbc8997fd60c2095a9f5358e87a90
Maksin_Andreev <andrianov_maksim@outlook.com>
2022-05-15 23:33:17 +0300

    (*<>*)

--- Home_Work/Seminar_6/6_/Program.cs
+++ Home_Work/Seminar_6/6_/Program.cs
@@ -1,1 +1,53 @@
-﻿
+﻿// Спирально заполнить двумерный массив
+
+int GetValueNullNatural(string text)
+{
+    string getValue = String.Empty;
+    int value = 0;
+    bool correct = false;
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out value);
+        if (control == true)
+        {
+            if (value != 0)
+            {
+                correct = true;
+            }
+        }
+    }
+    return value;
+}
+
+void PrintTwoArray(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            System.Console.Write($"{massive[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillTwoDimensionalArray(int[,] mass)
+{
+    1   2  3 4
+    10 11 12 5
+    9   8  7 6
+}
+
+void Main()
+{
+    string enterM = "specify the number of array rows";
+    string enterN = "specify the number of array columns";
+    int M = GetValueNullNatural(enterM);
+    int N = GetValueNullNatural(enterN);
+    int[,] array = new int[M, N];
+    
+    PrintTwoArray(array);
+}
--- Home_Work/Seminar_6/Test_/Program.cs
+++ Home_Work/Seminar_6/Test_/Program.cs
@@ -1,13 +1,4 @@
-﻿// В двумерном массиве n×k заменить четные элементы на противоположные
-
-// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
-
-// В двумерном массиве показать позиции числа, заданного пользователем 
-// или указать, что такого элемента нет
-
-// В матрице чисел найти сумму элементов главной диагонали
-
-int GetValueNullNatural(string text)
+﻿int GetValueNullNatural(string text)
 {
     string getValue = String.Empty;
     int value = 0;
@@ -29,105 +20,15 @@ int GetValueNullNatural(string text)
     return value;
 }
 
-int GetValueOptions(string text)
-{
-    string getValue = String.Empty;
-    int value = 0;
-    bool correct = false;
-    System.Console.WriteLine("[1] - Заменить четные элементы на противоположные");
-    System.Console.WriteLine("[2] - Заменить элементы, у которых оба индекса чётные на их квадраты");
-    System.Console.WriteLine("[3] - Вывести позицию заданного числа");
-    System.Console.WriteLine("[4] - Сумма элементов главной диагонали");
-
-    System.Console.WriteLine("[5] - Завершить работу");
-    while (!correct)
-    {
-        bool control = false;
-        System.Console.Write($"Enter the {text}:");
-        getValue = System.Console.ReadLine()!;
-        control = int.TryParse(getValue, out value);
-        if (control == true)
-        {
-            if (value != 0 && value < 6) // Сколько позиций выбора + 1
-            {
-                correct = true;
-            }
-        }
-    }
-    return value;
-}
-
 void FillTwoDimensionalArray(int[,] mass)
 {
     for (int i = 0; i < mass.GetLength(0); i++)
     {
         for (int j = 0; j < mass.GetLength(1); j++)
         {
-            mass[j, i] = new System.Random().Next(0, 10);
+            mass[i, j] = new System.Random().Next(0, 10);
         }
     }
-}
-
-void ReplacementElementOpposite(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (matrix[j, i] % 2 == 0)
-            {
-                matrix[j, i] *= -1;
-            }
-        }
-    }
-}
-
-void ReplacementElementIndexEven(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (j % 2 == 0 && i % 2 == 0)
-            {
-                matrix[i, j] *= matrix[j, i];
-            }
-        }
-    }
-}
-
-string SearchNumber(int[,] matrix, int search)
-{
-    string total = String.Empty;
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (matrix[j, i] == search)
-            {
-                return total = $"rows: {j}, columns: {i}";
-            }
-            else
-            {
-                total = "There is no such number(((";
-            }
-        }
-    }
-    return total;
-}
-
-int SumElementsMainDiagonal(int[,] mass)
-{
-    int i = mass.GetLength(0);// (arg1 - 1)(arg2 - 1) int arg1, int arg2
-    int j = 0;
-    int sumElements = 0;
-    while(j <= mass.GetLength(1))
-    {
-        sumElements = sumElements + mass[i,j];
-        j++;
-        i--;
-    }
-    return sumElements;
 }
 
 void PrintTwoArray(int[,] massive)
@@ -142,19 +43,111 @@ void PrintTwoArray(int[,] massive)
     }
 }
 
+int GetValueOptions(string text, int ind)
+{
+    ind++;
+    string getValue = String.Empty;
+    int valueText = 0;
+    bool correct = false;
+    System.Console.WriteLine("[1] - Заменить четные элементы на противоположные");
+    System.Console.WriteLine("[2] - Заменить элементы, у которых оба индекса чётные на их квадраты");
+    System.Console.WriteLine("[3] - Вывести позицию заданного числа");
+    // System.Console.WriteLine("[4] - Сумма элементов главной диагонали");
+    System.Console.WriteLine("[4] - Завершить работу");
+    while (!correct)
+    {
+        bool control = false;
+        System.Console.Write($"Enter the {text}:");
+        getValue = System.Console.ReadLine()!;
+        control = int.TryParse(getValue, out valueText);
+        if (control == true)
+        {
+            if (valueText != 0 && valueText < ind)
+            {
+                correct = true;
+            }
+        }
+    }
+    return valueText;
+}
+
+void ReplacementElementOpposite(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] % 2 == 0 && matrix[i, j] != 0)
+            {
+                matrix[i, j] *= -1;
+            }
+        }
+    }
+}
+
+void ReplacementElementIndexEven(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j % 2 == 0 && i % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+}
+
+string SearchNumber(int[,] matrix, int search)
+{
+    string total = String.Empty;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == search)
+            {
+                return total = $"rows: {i}, columns: {j}";
+            }
+            else
+            {
+                total = "There is no such number(((";
+            }
+        }
+    }
+    return total;
+}
+
+// int SumElementsMainDiagonal(int[,] mass)
+// {
+//     int i = mass.GetLength(0);// (arg1 - 1)(arg2 - 1) int arg1, int arg2
+//     int j = 0;
+//     int sumElements = 0;
+//     while(i != 0)
+//     {
+//         sumElements = sumElements + mass[i,j];
+//         j++;
+//         i--;
+//     }
+//     return sumElements;
+// }
+
 void Main()
 {
-    int indicator = 5; // Сколько позиций выбора. Не забудь изменить метод
     string enterM = "specify the number of array rows";
     string enterN = "specify the number of array columns";
-    string enterValue = "";
     int M = GetValueNullNatural(enterM);
     int N = GetValueNullNatural(enterN);
-    int[,] array = new int[M,N];
+    int[,] array = new int[M, N];
+    
     FillTwoDimensionalArray(array);
     PrintTwoArray(array);
-    int value = System.Convert.ToInt32(GetValueOptions(enterValue));
-    while (value != indicator)
+
+    int indicator = 4;
+    string enterValue = "";
+    int value = GetValueOptions(enterValue, indicator);
+    while (value < indicator)
     {
         if (value == 1)
         {
@@ -178,16 +171,17 @@ void Main()
             int numberSearch = GetValueNullNatural(enterNumber);
             System.Console.WriteLine(SearchNumber(array, numberSearch));
         }
-        if (value == 4)
-        {
-            DateTime start = DateTime.Now;
-            System.Console.WriteLine(SumElementsMainDiagonal(array));
-            double running = (DateTime.Now - start).TotalMilliseconds;
-            System.Console.WriteLine($"Program_running_time: {running}");
-        }
-        value = System.Convert.ToInt32(GetValueOptions(enterValue));
+        // if (value == 4)
+        // {
+        //     DateTime start = DateTime.Now;
+        //     System.Console.WriteLine(SumElementsMainDiagonal(array));
+        //     double running = (DateTime.Now - start).TotalMilliseconds;
+        //     System.Console.WriteLine($"Program_running_time: {running}");
+        // }
+        value = GetValueOptions(enterValue, indicator);
     }
     System.Console.WriteLine("Thank you!");
+
 }
 
 Main();
