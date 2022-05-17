19aa62f34ca603579db1853a55b34073cd9ec149
Maksin_Andreev <andrianov_maksim@outlook.com>
2022-05-17 11:13:46 +0300

    (*<>*)

--- Home_Work/Seminar_6/6_/Program.cs
+++ Home_Work/Seminar_6/6_/Program.cs
@@ -37,8 +37,10 @@ void PrintTwoArray(int[,] massive)
 void FillTwoDimensionalArray(int[,] mass)
 {
     // 1   2  3 4
-    // 10 11 12 5
-    // 9   8  7 6
+    // 12 13 14 5
+    // 11 16 15 6
+    // 10  9 8  7
+    
 }
 
 void Main()
