a88df5149475a5ea61b6916e6f6ec270d54ad855
Maksim_Andreev <andrianov_maksim@outlook.com>
2022-05-20 20:55:19 +0300

    FillTwoDimensionalArray

--- Project/Print_Array/Program.cs
+++ Project/Print_Array/Program.cs
@@ -6,7 +6,7 @@ string PrintArrayIf(string[] printarray)
     string print = string.Empty;
     for (int i = 0; i < printarray.Length; i++)
     {
-        print += $"{printarray[i]}";
+        print += $"{printarray[i]} ";
         print += System.Environment.NewLine;
     }
     return print;
